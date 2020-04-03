31c10961af197d588a8e962db0e1d9122b1ca779
mnzlucas <mnzlucas7@gmail.com>
2020-04-03 20:47:49 -0300

    front- end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,34 @@
+/**
+ * Letras maiúsculas no começo dos nomes das pastas
+ * se não, não funciona
+ * React precisa ser utilizado em JSX - html em aquivo js
+ *  sempre que for utilizar js dentro do html precisa utilizar {]
+ * 
+ * uma das maneiras de declarar <Header title="Semana OmniStack"/>
+ *
+ * conceito de estado, é uma informação que necessita ser alterada 
+ * com clicks, incrementos entre outros 
+ * 
+ * conceito de Imutabilidade, não se pode mudar o valor da variavel
+ * diretamente, precisa sobrepor com metodo {useState}
+ * la tem uma função de alteração.
+ * 
+ * sempre que precisar armazenar valores, criaremos estados.
+ * 
+ * JSX - java script xml
+ * uma das maneiras <Header title="Semana OmniStack"/>
+ */
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          A mae mandou voce ir dobrar
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
-export default App;
+  export default App;
